f35fd95fbd7b989beff6e13d03c5fb731f12d401
Aram <yeghiazaryan99@gmail.com>
2019-01-13 23:21:29 +0400

    my 3 entities

---
+++ src/main/java/am/aca/quiz_software/entities/ScoreEntity.java
@@ -0,0 +1,69 @@
+package am.aca.quiz_software.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "average_score")
+public class ScoreEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "score_value",nullable = false,columnDefinition = "0")
+    private double value;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private TopicEntity topic;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private UserEntity userEntity;
+
+    public ScoreEntity(long id, double value, TopicEntity topic, UserEntity userEntity) {
+        this.id = id;
+        this.value = value;
+        this.topic = topic;
+        this.userEntity = userEntity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public TopicEntity getTopic() {
+        return topic;
+    }
+
+    public void setTopic(TopicEntity topic) {
+        this.topic = topic;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    @Override
+    public String toString() {
+        return "ScoreEntity{" +
+                "id=" + id +
+                ", value=" + value +
+                ", topic=" + topic +
+                ", userEntity=" + userEntity +
+                '}';
+    }
+}
