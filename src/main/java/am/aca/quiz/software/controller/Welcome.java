103370cb439decceca1bf4f057581cf93be090aa
Tigran Gyozalyan <tiko.gyozalyan@gmail.com>
2019-01-31 02:38:12 +0400

    fixed questions.html toDo fix the post controller

--- src/main/java/am/aca/quiz/software/controller/Welcome.java
+++ src/main/java/am/aca/quiz/software/controller/Welcome.java
@@ -1,6 +1,6 @@
 package am.aca.quiz.software.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
