bd70c1b346dccad2699c470228a3604cebef9231
Edgar Ohanyan <edgarohanyan@gmail.com>
2019-02-02 16:19:30 +0400

    disable csrf

--- src/main/java/am/aca/quiz/software/config/WebSecurityConfig.java
+++ src/main/java/am/aca/quiz/software/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                 .httpBasic();
     }
 
     @Override
