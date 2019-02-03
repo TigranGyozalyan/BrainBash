46d0cb1740765e88ce31aa7045172c0d345208f3
Aram <yeghiazaryan99@gmail.com>
2019-02-03 19:42:42 +0400

    Not Found

--- src/main/java/am/aca/quiz/software/controller/Welcome.java
+++ src/main/java/am/aca/quiz/software/controller/Welcome.java
@@ -1,10 +1,15 @@
 package am.aca.quiz.software.controller;
 
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
 
 @RestController
 @RequestMapping("/")
@@ -12,10 +17,18 @@ public class Welcome {
 
     @GetMapping
     public ModelAndView welcomePage(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         ModelAndView modelAndView=new ModelAndView("welcome");
-        return modelAndView;
+        if(authentication instanceof AnonymousAuthenticationToken){
+            return modelAndView;
+        }
+        return new ModelAndView("redirect:/notfound");
+
     }
 
-
+    @GetMapping(value = "notfound")
+    public ModelAndView notFound(){
+        return new ModelAndView("notFound");
+    }
 
 }
