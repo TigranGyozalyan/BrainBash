be21ec18da699e0a624f4cf40d31451ffd91837f
Edgar Ohanyan <edgarohanyan@gmail.com>
2019-02-06 01:47:24 +0400

    a

--- src/main/java/am/aca/quiz/software/controller/HistoryController.java
+++ src/main/java/am/aca/quiz/software/controller/HistoryController.java
@@ -1,8 +1,8 @@
 package am.aca.quiz.software.controller;
 
-import am.aca.quiz.software.entity.UserEntity;
 import am.aca.quiz.software.entity.enums.Status;
 import am.aca.quiz.software.service.implementations.HistoryServiceImp;
+import am.aca.quiz.software.service.implementations.TestServiceImp;
 import am.aca.quiz.software.service.implementations.UserServiceImp;
 import am.aca.quiz.software.service.mapper.HistoryMapper;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,22 +19,39 @@ public class HistoryController {
     private final HistoryServiceImp historyServiceImp;
     private final HistoryMapper historyMapper;
     private final UserServiceImp userServiceImp;
+    private final TestServiceImp testServiceImp;
 
-    public HistoryController(HistoryServiceImp historyServiceImp, HistoryMapper historyMapper, UserServiceImp userServiceImp) {
+    public HistoryController(HistoryServiceImp historyServiceImp, HistoryMapper historyMapper, UserServiceImp userServiceImp, TestServiceImp testServiceImp) {
         this.historyServiceImp = historyServiceImp;
         this.historyMapper = historyMapper;
         this.userServiceImp = userServiceImp;
+        this.testServiceImp = testServiceImp;
     }
 
     @GetMapping("/pastList")
     public ModelAndView pastHistoryList(Principal principal) {
         ModelAndView modelAndView = new ModelAndView("pastHistory");
-
         try {
             Long userId = userServiceImp.findByEmail(principal.getName()).getId();
 
             modelAndView.addObject("historyList", historyMapper.mapEntitiesToDto(historyServiceImp.findAllByStatus(userId, Status.COMPLETED)));
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/allHistory")
+    public ModelAndView userAllHistory(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("allHistory");
+
+        try {
+            Long userId = userServiceImp.findByEmail(principal.getName()).getId();
+
+//            String testName = testServiceImp.getById().getTest_name();
+
+            modelAndView.addObject("historyList", historyMapper.mapEntitiesToDto(historyServiceImp.findAllByUserId(userId)));
         } catch (SQLException e) {
             e.printStackTrace();
         }
